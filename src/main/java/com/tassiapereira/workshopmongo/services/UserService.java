b059dd42ec4d7940a79ad1af7caf7077abc5fa98
Tassia Pereira <tassiapereira2020@gmail.com>
2024-05-22 15:36:11 -0300

    conectando ao mongoDB com repository e service

---
+++ src/main/java/com/tassiapereira/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.tassiapereira.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tassiapereira.workshopmongo.domain.User;
+import com.tassiapereira.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}

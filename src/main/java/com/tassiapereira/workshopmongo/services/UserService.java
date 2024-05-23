e6da7643a9da238b7ae112410e2fc45e9482e08f
Tassia Pereira <tassiapereira2020@gmail.com>
2024-05-23 19:58:54 -0300

    insercao de usuario com POST

--- src/main/java/com/tassiapereira/workshopmongo/services/UserService.java
+++ src/main/java/com/tassiapereira/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tassiapereira.workshopmongo.domain.User;
+import com.tassiapereira.workshopmongo.dto.UserDTO;
 import com.tassiapereira.workshopmongo.repository.UserRepository;
 import com.tassiapereira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,18 @@ public class UserService {
 		}
 		return user;
 	}
+	
+	//inserindo um objeto
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
+		
+	}
+	
+		
 
 }

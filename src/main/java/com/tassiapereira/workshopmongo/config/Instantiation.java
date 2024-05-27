c55bd7b7ace7d46be767369ba8bfac09e064b9bf
Tassia Pereira <tassiapereira2020@gmail.com>
2024-05-27 15:50:37 -0300

    criando entity post com user aninhado

--- src/main/java/com/tassiapereira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tassiapereira/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.tassiapereira.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.tassiapereira.workshopmongo.domain.Post;
 import com.tassiapereira.workshopmongo.domain.User;
+import com.tassiapereira.workshopmongo.repository.PostRepository;
 import com.tassiapereira.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,19 +19,30 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM//yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		//zerar a colecao no mongoDb
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Silva", "silva@gmail.com");
 		User jorge = new User(null, "Jorge Amado", "amado@gmail.com");
 		User saulo = new User(null, "Saulo Medeiros", "ss@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/02/2024"), "partiu viagem", "vou viajar para o nordeste, abracos", maria);
+		Post post2 = new Post(null, sdf.parse("23/02/2024"), "bom dia", "hoje eu acordei animado", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, jorge, saulo));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/tassiapereira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package com.tassiapereira.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tassiapereira.workshopmongo.domain.Post;
+import com.tassiapereira.workshopmongo.domain.User;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+	User findOne(String id);
+
+	
+	
+	
+
+}
